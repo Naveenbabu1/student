84996f1e9e58efba20d00f7a6e13f0299553ea37
Naveen Babu <70095369+Naveenbabu1@users.noreply.github.com>
2021-12-17 11:36:22 +0530

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -148,9 +148,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER ="mi5907530@gmail.com"
+EMAIL_HOST_USER =""
     #os.environ.get('EMAIL_ADDRESS')
-EMAIL_HOST_PASSWORD ="Aknusms@19"
+EMAIL_HOST_PASSWORD =""
     #os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Adikavi Nannaya University'
